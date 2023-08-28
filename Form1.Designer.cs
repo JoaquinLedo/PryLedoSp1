27d2d96562a703e6db6c10c2aec7d44f7db7fc67
root <mgiraudo@ies21.edu.ar>
2023-08-28 13:58:58 -0300

    crea el archivo, lo graba y lo lee

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,20 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.BtnArchivo = new System.Windows.Forms.Button();
+            this.BtnLeer = new System.Windows.Forms.Button();
+            this.BtnGrabar = new System.Windows.Forms.Button();
+            this.Txt = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // BtnArchivo
+            // 
+            this.BtnArchivo.Location = new System.Drawing.Point(12, 221);
+            this.BtnArchivo.Name = "BtnArchivo";
+            this.BtnArchivo.Size = new System.Drawing.Size(114, 37);
+            this.BtnArchivo.TabIndex = 0;
+            this.BtnArchivo.Text = "Crear Archivo";
+            this.BtnArchivo.UseVisualStyleBackColor = true;
+            this.BtnArchivo.Click += new System.EventHandler(this.BtnArchivo_Click);
+            // 
+            // BtnLeer
+            // 
+            this.BtnLeer.Location = new System.Drawing.Point(181, 221);
+            this.BtnLeer.Name = "BtnLeer";
+            this.BtnLeer.Size = new System.Drawing.Size(114, 37);
+            this.BtnLeer.TabIndex = 1;
+            this.BtnLeer.Text = "Leer Archivo";
+            this.BtnLeer.UseVisualStyleBackColor = true;
+            this.BtnLeer.Click += new System.EventHandler(this.BtnLeer_Click);
+            // 
+            // BtnGrabar
+            // 
+            this.BtnGrabar.Location = new System.Drawing.Point(96, 178);
+            this.BtnGrabar.Name = "BtnGrabar";
+            this.BtnGrabar.Size = new System.Drawing.Size(114, 37);
+            this.BtnGrabar.TabIndex = 2;
+            this.BtnGrabar.Text = "Grabar";
+            this.BtnGrabar.UseVisualStyleBackColor = true;
+            this.BtnGrabar.Click += new System.EventHandler(this.BtnGrabar_Click);
+            // 
+            // Txt
+            // 
+            this.Txt.Location = new System.Drawing.Point(36, 12);
+            this.Txt.Name = "Txt";
+            this.Txt.Size = new System.Drawing.Size(236, 20);
+            this.Txt.TabIndex = 3;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(686, 439);
+            this.ClientSize = new System.Drawing.Size(307, 270);
+            this.Controls.Add(this.Txt);
+            this.Controls.Add(this.BtnGrabar);
+            this.Controls.Add(this.BtnLeer);
+            this.Controls.Add(this.BtnArchivo);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button BtnArchivo;
+        private System.Windows.Forms.Button BtnLeer;
+        private System.Windows.Forms.Button BtnGrabar;
+        private System.Windows.Forms.TextBox Txt;
     }
 }
 
--- Frm1.cs
+++ Frm1.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.IO;
+
 namespace PryLedoSp1
 {
     public partial class Form1 : Form
@@ -15,6 +17,57 @@ namespace PryLedoSp1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void BtnArchivo_Click(object sender, EventArgs e)
+        {
+            try 
+            {
+                StreamWriter swCrearArchivo = new StreamWriter("MiArchivito", false);
+                MessageBox.Show("Archivo Creado");
+
+                
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("ERROR");
+            }
+
+
+        }
+
+        private void BtnGrabar_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                StreamWriter swCrearArchivo = new StreamWriter("MiArchivito", true);
+                swCrearArchivo.WriteLine(Txt.Text);
+                swCrearArchivo.Close();
+                MessageBox.Show("Guardando");
+
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("ERROR");
+            }
+
+        }
+
+        private void BtnLeer_Click(object sender, EventArgs e)
+        {
+            StreamReader swCrearArchivo = new StreamReader("MiArchivito");
+
+            while(swCrearArchivo.EndOfStream==false)
+            {
+                Txt.Text += swCrearArchivo.ReadLine();
+            }
         }
     }
 }
